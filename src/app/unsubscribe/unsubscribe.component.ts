4950489c8fce262d26bff9ebe1f7b6c30a330862
gitmbints <rahmbints@gmail.com>
2024-04-28 22:23:30 +0300

    #80 Unsubscribe from an Observable

--- src/app/unsubscribe/unsubscribe.component.ts
+++ src/app/unsubscribe/unsubscribe.component.ts
@@ -7,5 +7,46 @@ import { interval } from 'rxjs';
   styleUrls: ['./unsubscribe.component.css']
 })
 export class UnsubscribeComponent {
-  
+  counter = interval(1000);
+
+  data1: number[] = [];
+  data2: number[] = [];
+  data3: number[] = [];
+
+  subscriber1;
+  subscriber2;
+  subscriber3;
+
+  onSubscribe1() {
+    this.subscriber1 = this.counter.subscribe((data) => {
+      this.data1.push(data);
+    });
+  }
+
+  onUnsubscribe1() {
+    this.subscriber1.unsubscribe();
+    this.data1.splice(0, this.data1.length);
+  }
+
+  onSubscribe2() {
+    this.subscriber2 = this.counter.subscribe((data) => {
+      this.data2.push(data);
+    });
+  }
+
+  onUnsubscribe2() {
+    this.subscriber2.unsubscribe();
+    this.data2.splice(0, this.data2.length);
+  }
+
+  onSubscribe3() {
+    this.subscriber3 = this.counter.subscribe((data) => {
+      this.data3.push(data);
+    });
+  }
+
+  onUnsubscribe3() {
+    this.subscriber3.unsubscribe();
+    this.data3.splice(0, this.data3.length);
+  }
 }
